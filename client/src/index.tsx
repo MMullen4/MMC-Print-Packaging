f5b8aca069b7153eb122af1ea02b66813b65fcc2
Matt Mullen <mhmullen@icloud.com>
2025-08-07 11:12:16 -0700

    revised .env & pkg.json files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ApolloProvider } from "@apollo/client";
 import client from "./graphql/client"; // 👈 this is the important part
-import "./index.css";
+import "./index.css"; // import Tailwind CSS styles
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
